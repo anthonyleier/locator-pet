481620b2347ee25c21d7fc392c9458e4229471ed
Anthony Cruz <anthonyleielw@gmail.com>
2022-09-22 17:33:02 -0300

    Model de post e conexão com Postgres

--- localizador/models.py
+++ localizador/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=65)
+    descricao = models.CharField(max_length=165)
+    slug = models.SlugField()
+    status = models.CharField(max_length=15)
+    publicado = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    imagem1 = models.ImageField(upload_to='localizador/upload/%Y/%m/%d/')
+    imagem2 = models.ImageField(upload_to='localizador/upload/%Y/%m/%d/')
+    imagem3 = models.ImageField(upload_to='localizador/upload/%Y/%m/%d/')
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
