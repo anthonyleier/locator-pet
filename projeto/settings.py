6db05d12ecd868c457f20d0988b2a7ef43c08b2c
Anthony Cruz <anthonyleielw@gmail.com>
2022-09-23 16:33:36 -0300

    Localização

--- projeto/settings.py
+++ projeto/settings.py
@@ -106,12 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'PT-BR'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
-
 USE_TZ = True
 
 
