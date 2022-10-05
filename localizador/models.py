efbffbbe25e32765cd06cda82e22134a3fc62db9
Anthony Cruz <anthonyleielw@gmail.com>
2022-10-05 15:28:21 -0300

    Uso de slugs unicos

--- localizador/models.py
+++ localizador/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     titulo = models.CharField(max_length=65)
     descricao = models.CharField(max_length=165)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     status = models.CharField(max_length=15)
     publicado = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
