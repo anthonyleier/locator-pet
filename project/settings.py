e32717fb3d06de92e30925a6271089ace20d1bd7
Anthony Cruz <anthonyleielw@gmail.com>
2022-10-10 16:03:32 -0300

    Flash messages configuration

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,3 +110,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    constants.SUCCESS: 'alert-success',
+    constants.WARNING: 'alert-warning',
+    constants.ERROR: 'alert-danger',
+    constants.DEBUG: 'alert-secondary',
+    constants.INFO: 'alert-info',
+}
