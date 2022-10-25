fa9d85dd4da94dac408c0c5c25b6d5e124349c68
Anthony Cruz <anthonyleielw@gmail.com>
2022-10-25 11:19:53 -0300

    Better layout for login and register

--- locator/forms.py
+++ locator/forms.py
@@ -6,9 +6,13 @@ from utils.functions import addAttr, addPlaceholder, verifyStrongPassword
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(
-        widget=forms.PasswordInput()
-    )
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    addAttr(username, 'class', 'form-control')
+    addAttr(password, 'class', 'form-control')
+
+    username.label = 'Usuário'
+    password.label = 'Senha'
 
 
 class RegisterForm(forms.ModelForm):
@@ -28,8 +32,8 @@ class RegisterForm(forms.ModelForm):
         addPlaceholder(self.fields['last_name'], 'Ex.: Smith')
         addPlaceholder(self.fields['username'], 'Ex.: jonh.smith')
         addPlaceholder(self.fields['email'], 'Ex.: jonh.smith@gmail.com')
-        addPlaceholder(self.fields['password'], 'Your password')
-        addPlaceholder(self.fields['password_confirm'], 'Your password again')
+        addPlaceholder(self.fields['password'], 'Sua senha')
+        addPlaceholder(self.fields['password_confirm'], 'Sua senha novamente')
 
         addAttr(self.fields['first_name'], 'class', 'form-control')
         addAttr(self.fields['last_name'], 'class', 'form-control')
@@ -41,33 +45,32 @@ class RegisterForm(forms.ModelForm):
     password = forms.CharField(
         required=True,
         widget=forms.PasswordInput,
-        error_messages={'required': 'Password must not be empty'},
-        help_text=('Password must have at least one uppercase letter, one lowercase letter and one number. The length should be at least 8 characters.'),
+        error_messages={'required': 'Senha não pode ser vazia.'},
+        help_text=('Necessário conter uma letra minúscula, uma letra maiúscula e um número. Tamanho mínimo de 8 caracteres.'),
         validators=[verifyStrongPassword],
-        label='Password'
+        label='Senha'
     )
 
     password_confirm = forms.CharField(
         required=True,
         widget=forms.PasswordInput,
-        error_messages={'required': 'Password must not be empty'},
-        help_text=('Password must be equal'),
-        validators=[verifyStrongPassword],
-        label='Password confirm'
+        error_messages={'required': 'Confirmação de senha não pode ser vazia.'},
+        label='Confirmação de senha'
     )
 
-    def clean(self):
-        cleaned_data = super().clean()
-        email = cleaned_data.get('email')
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
         exists = User.objects.filter(email=email)
 
         if exists:
-            error = ValidationError('Email tem que ser diferente')
-            raise ValidationError({'email': error})
+            raise ValidationError('Este email já está em uso.', code='invalid')
+
+    def clean(self):
+        cleaned_data = super().clean()
 
         password = cleaned_data.get('password')
         password_confirm = cleaned_data.get('password_confirm')
 
         if password != password_confirm:
-            error = ValidationError('Password and password validation must be the same', code='invalid')
+            error = ValidationError('As senhas devem ser iguais.', code='invalid')
             raise ValidationError({'password': error, 'password_confirm': error})
--- utils/functions.py
+++ utils/functions.py
@@ -16,4 +16,4 @@ def verifyStrongPassword(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
 
     if not regex.match(password):
-        raise ValidationError(('Fracokk'), code='invalid')
+        raise ValidationError(('Sua senha é muito fraca.'), code='invalid')
