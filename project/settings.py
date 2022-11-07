1a6e7713de9d3ace89776c713100b3d70f502c6a
Anthony Cruz <anthonyleielw@gmail.com>
2022-11-07 17:45:19 -0300

    Debug default config and allowed hosts in dotenv

--- project/settings.py
+++ project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('PUBLIC_URL')]
 
 # Application definition
 INSTALLED_APPS = [
