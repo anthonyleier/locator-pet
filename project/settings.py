e9996808653299f4510ff883c6253df1639c1f96
Anthony Cruz <anthonyleielw@gmail.com>
2022-11-10 16:37:05 -0300

    Allowed Hosts

--- project/settings.py
+++ project/settings.py
@@ -13,8 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
-ALLOWED_HOSTS = [os.environ.get('PUBLIC_URL')]
-CSRF_TRUSTED_ORIGINS = [os.environ.get('PUBLIC_URL')]
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 # Application definition
 INSTALLED_APPS = [
