f2fba0944688394d45751c3ad95fe4ad1edd7749
Anthony Cruz <anthonyleielw@gmail.com>
2022-11-11 11:54:17 -0300

    Validation for POST Method

--- locator/serializers.py
+++ locator/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from locator.models import Post
 
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=65)
-    description = serializers.CharField(max_length=165)
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title', 'description', 'slug', 'status', 'published', 'created_at', 'updated_at', 'image1', 'image2', 'image3', 'author']
+
     author = serializers.StringRelatedField()
+    published = serializers.BooleanField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
--- locator/views/api.py
+++ locator/views/api.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from django.shortcuts import get_object_or_404
 
@@ -7,11 +8,20 @@ from locator.models import Post
 from locator.serializers import PostSerializer
 
 
-@api_view(http_method_names=['GET'])
+@api_view(http_method_names=['GET', 'POST'])
 def postsList(request):
-    posts = Post.objects.all().select_related('author')
-    serializer = PostSerializer(instance=posts, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        posts = Post.objects.all().select_related('author')
+        serializer = PostSerializer(instance=posts, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(http_method_names=['GET'])
