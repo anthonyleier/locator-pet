a96560f4a34b8a68eb3ab298d93fe55062d2a19c
Anthony Cruz <anthonyleielw@gmail.com>
2022-11-14 10:29:05 -0300

    Update and delete for API

--- locator/serializers.py
+++ locator/serializers.py
@@ -5,7 +5,7 @@ from locator.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['title', 'description', 'slug', 'status', 'published', 'created_at', 'updated_at', 'image1', 'image2', 'image3', 'author']
+        fields = ['id', 'title', 'description', 'slug', 'status', 'published', 'created_at', 'updated_at', 'image1', 'image2', 'image3', 'author']
 
     author = serializers.StringRelatedField()
     published = serializers.BooleanField(read_only=True)
@@ -20,7 +20,7 @@ class PostSerializer(serializers.ModelSerializer):
         return title
 
     def validate(self, attrs):
-        superValidate = super().clean(attrs)
+        superValidate = super().validate(attrs)
         errors = []
 
         title = attrs.get('title')
@@ -32,3 +32,5 @@ class PostSerializer(serializers.ModelSerializer):
 
         if errors:
             raise serializers.ValidationError(errors)
+
+        return superValidate
